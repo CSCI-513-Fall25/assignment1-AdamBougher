df32992c59dc3ae0890e21803805d4f53000ee95
AdamBougher <a.bougher1999@gmail.com>
2025-09-29 21:00:16 -0500

    now using new DS's

--- Assignment1/assignment1/src/main/java/com/adambougher/Main.java
+++ Assignment1/assignment1/src/main/java/com/adambougher/Main.java
@@ -8,7 +8,8 @@ import java.util.Vector;
 public class Main {
     public static void main(String[] args) {
         String fileName = "Assignment1\\assignment1\\src\\input.txt";
-        Vector<Integer> coordinates = new Vector<Integer>();
+        Vector<Pair> coordinates = new Vector<Pair>();
+        Vector<Ship> ships;
         Vector<Vector<Ship>> games = new Vector<Vector<Ship>>();
         BattleshipSearch bs;
         
@@ -19,26 +20,35 @@ public class Main {
 			StringTokenizer st;
 			String line;
 
+			
+
 			while((line = reader.readLine()) != null) {
 				st = new StringTokenizer(line, "(,)");
-                games.add(new Vector<Ship>());
+                ships = new Vector<Ship>();
+
                 for(int i =0; i < 5; i++){
-                    coordinates.add(Integer.parseInt(st.nextToken()));
-                    coordinates.add(Integer.parseInt(st.nextToken()));
+                    Pair p = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                    coordinates.add(p);
                 }
-                games.lastElement().add(new Ship(coordinates,5));
+
+                ships.add(new Ship(coordinates, ShipType.CARRIER));
                 coordinates.clear();
+
                 for(int i =0; i < 3; i++){
-                    coordinates.add(Integer.parseInt(st.nextToken()));
-                    coordinates.add(Integer.parseInt(st.nextToken()));
+                    coordinates.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
                 }
-                games.lastElement().add(new Ship(coordinates,3));
+
+                ships.add(new Ship(coordinates, ShipType.SUBMARINE));
                 coordinates.clear();
+
+                games.add(ships);
 			}
 			reader.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+
 
         for (Vector<Ship> game : games) {
             bs = new BattleshipSearch(new HorizontalSweep(), game.get(0), game.get(1));
