5e4286d50de7b5bc56954d4e31f8f3c2f2b76602
AdamBougher <a.bougher1999@gmail.com>
2025-09-29 21:24:12 -0500

    adjusted location of break checks

--- Assignment1/assignment1/src/main/java/com/adambougher/HorizontalSweep.java
+++ Assignment1/assignment1/src/main/java/com/adambougher/HorizontalSweep.java
@@ -6,22 +6,23 @@ public class HorizontalSweep extends SearchStrategy {
     public void FindShip(GameGrid grid) {
         Name = "Horizontal Sweep";
         NumOfCells = 0;
-        shipCoordinates = new Vector<Vector<int[][]>>(25,25);
+        shipCoordinates = new Vector<Pair>();
         PrintName();
+
         for (int row = 0; row < grid.getGrid().length; row++) {
-            if (shipCoordinates.size() >= 8) {
-                break;
-            }
 
             for (int col = 0; col < grid.getGrid()[row].length; col++) {
+                NumOfCells++;
+                if (grid.CheckPosition(row, col)) {
+                    shipCoordinates.add(new Pair(row, col));
+                }
+
                 if (shipCoordinates.size() >= 8) {
                     break;
                 }
-
-                NumOfCells++;
-                if (grid.CheckPosition(row, col)) {
-                    shipCoordinates.add(new Vector<int[][]>(row, col));
-                }
+            }
+            if (shipCoordinates.size() >= 8) {
+                break;
             }
         }
         PrintNumOfCells();
