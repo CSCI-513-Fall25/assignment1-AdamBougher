684a502300c7a162509134dedbd456a7d7c2442d
Adam Bougher <adamsauce99@gmail.com>
2025-10-21 09:42:48 -0500

    java things

---
+++ Assignment3/gardenlayout/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.adambougher {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.graphics;
+    requires javafx.base;
+    opens com.adambougher to javafx.fxml;
+    exports com.adambougher;
+}
