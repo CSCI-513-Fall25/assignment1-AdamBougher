52c6b66895592562b18b09c779dcbdb66ffc94c1
Adam Bougher <adamsauce99@gmail.com>
2025-10-21 10:18:01 -0500

    added containspoint implamentation

--- Assignment3/gardenlayout/src/main/java/com/adambougher/flower.java
+++ Assignment3/gardenlayout/src/main/java/com/adambougher/flower.java
@@ -1,24 +1,22 @@
 package com.adambougher;
 
 import javafx.geometry.Point2D;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
 
 public class flower implements moveable {
-    Circle circle;
+    javafx.scene.shape.Circle circle;
     Point2D position;
     boolean movable;
     double radius = 10;
 
 
-    public flower(Point2D position, Color color, boolean movable) {
-        circle = new Circle();
+    public flower(Point2D position, javafx.scene.paint.Color color, boolean movable) {
+        circle = new javafx.scene.shape.Circle();
         circle.setCenterX(position.getX());
         circle.setCenterY(position.getY());
         circle.setRadius(radius);  
         circle.setFill(color);  
-        circle.setStroke(Color.BLACK);  
+        circle.setStroke(javafx.scene.paint.Color.BLACK);  
         circle.setStrokeWidth(1);
 
         this.position = position;
@@ -35,8 +33,7 @@ public class flower implements moveable {
 
     @Override
     public boolean ContainsPoint(Point2D point) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ContainsPoint'");
+        return (circle.contains(point));
     }
 
 
